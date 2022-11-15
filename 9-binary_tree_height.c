09fd19b8e3e115e950252dccbc143e5332f600a4
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-15 15:46:47 +0300

    t9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,20 +13,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
+	if (tree->left)
 	{
-		if (tree->left)
-		{
-			hl = 1 + binary_tree_height(tree->left);
-		}
-		if (tree->right)
-		{
-			hr = 1 + binary_tree_height(tree->right);
-		}
-		
-		if (hl > hr)
-			return (hl);
-		else
-			return (hr);
+		hl = 1 + binary_tree_height(tree->left);
 	}
+	if (tree->right)
+	{
+		hr = 1 + binary_tree_height(tree->right);
+	}
+
+	if (hl > hr)
+		return (hl);
+	else
+		return (hr);
 }
