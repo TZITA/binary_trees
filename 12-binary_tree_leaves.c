52880134b20619d1dc3fda36ef48cb3ae0c0c4c0
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-16 22:52:46 +0300

    t12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,9 +8,15 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t l = 0;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->left && tree->right)
-		return (2);
-	return (1);
+
+	if (!tree->left && !tree->right)
+		l += 1;
+	l += binary_tree_leaves(tree->left);
+	l += binary_tree_leaves(tree->right);
+
+	return (l);
 }
