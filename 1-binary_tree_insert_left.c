26c0eb239d82172bc1dfd07a82543f69039860de
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-16 18:35:26 +0300

    t1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *tmp;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL)
+	if (node == NULL || parent == NULL || parent->left == NULL)
 	{
 		return (NULL);
 	}
