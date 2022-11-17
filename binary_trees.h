f9fd1867dac0f1ea64a1d8c1cad7467ac69fd3c6
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-17 18:54:58 +0300

    t103

--- binary_trees.h
+++ binary_trees.h
@@ -38,5 +38,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif /*BINARY_TREE_H*/
